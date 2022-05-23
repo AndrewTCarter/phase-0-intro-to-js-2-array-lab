1c719776bba3065e2aff0936efac324bf4a29da8
AndrewTCarter <Andrewkarter@gmail.com>
2022-05-23 16:11:45 -0400

    commit tree 9f8c299da84519cc275bbeac6ec96aea7fe5b25c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo',"Otis","Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const moreCats = [...cats, "Broom"]
+    return moreCats
+}
+
+function prependCat(name) {
+    const moreCats = ["Arnold",...cats]
+    return moreCats
+}
+
+function removeLastCat() {
+    const lessCats = cats.slice(0,2)
+    return lessCats
+}
+
+function removeFirstCat() {
+    const lessCats = cats.slice(1)
+    return lessCats
+}
